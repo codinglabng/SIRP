85884606e8c1e324001a077ab6a28aeae6148406
Project Manager <team@codinglab.ng>
2017-04-03 22:00:19 +0100

    pull project into this repo

---
+++ PRIS/PRIS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PRIS.Startup))]
+namespace PRIS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
