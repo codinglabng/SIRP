4b91b381f57bc5e3fefcfd7accd2da5313ef5d2f
Mr Francis <mrfrancis@codinglab.ng>
2017-04-06 18:12:23 +0100

    Add controller, view  to display available apps for user

---
+++ PRIS/PRIS.Core/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PRIS.Data.Models;
+
+namespace PRIS.Core
+{
+    public interface IUserRepository
+    {
+
+        App GetAppsByUserId(int Id);
+        IQueryable<App> GetApps();
+        
+
+    }
+}
---
+++ PRIS/PRIS.Repositories/UserRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PRIS.Core;
+using PRIS.Data.Models;
+
+namespace PRIS.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly PrisDbEntities _db = new PrisDbEntities();
+        public IQueryable<App> GetApps()
+        {
+            var apps = _db.Apps.OrderByDescending(e => e.Id);
+            return apps;
+        }
+
+        public App GetAppsByUserId(int Id)
+        {
+            var app = _db.Apps.FirstOrDefault(e => e.Id == Id);
+            return app;
+        }
+
+        
+    }
+}
