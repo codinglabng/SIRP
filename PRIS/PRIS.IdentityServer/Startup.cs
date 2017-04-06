c29eff927a9f8f615ae1e027f778acc8ffbadfe2
Project Manager <team@codinglab.ng>
2017-04-07 00:11:13 +0100

    Successfully implement Identity server and open id

--- PRIS/PRIS.IdentityServer/Startup.cs
+++ PRIS/PRIS.IdentityServer/Startup.cs
@@ -2,27 +2,15 @@
 using IdentityServer3.Core;
 using IdentityServer3.Core.Configuration;
 using Microsoft.Owin;
-using Owin;
-using PRIS.IdentityServer;
-using PRIS.IdentityServer.IdentityServer;
-using Microsoft.IdentityModel.Protocols;
-using Microsoft.Owin;
-using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
-using Microsoft.Owin.Security.OpenIdConnect;
 using Owin;
+using PRIS.IdentityServer.IdentityServer;
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens;
-using System.Security.Claims;
 using System.Security.Cryptography.X509Certificates;
-using System.Linq;
 using System.Web.Helpers;
-using IdentityServer3.Core;
-using IdentityServer3.Core.Configuration;
-using IdentityModel.Client;
-using System.Threading.Tasks;
 
 [assembly: OwinStartup(typeof(PRIS.IdentityServer.Startup))]
 
@@ -49,6 +37,7 @@ namespace PRIS.IdentityServer
                                     .UseInMemoryUsers(Users.Get())
                                     .UseInMemoryClients(Clients.Get())
                                     .UseInMemoryScopes(Scopes.Get()),
+                        RequireSsl = false,
 
                         AuthenticationOptions = new IdentityServer3.Core.Configuration.AuthenticationOptions
                         {
@@ -66,68 +55,68 @@ namespace PRIS.IdentityServer
             });
 
 
-            app.UseOpenIdConnectAuthentication(new OpenIdConnectAuthenticationOptions
-            {
-                Authority = "https://localhost:44319/identity",
+            //app.UseOpenIdConnectAuthentication(new OpenIdConnectAuthenticationOptions
+            //{
+            //    Authority = "https://localhost:44319/identity",
 
-                ClientId = "mvc",
-                Scope = "openid profile roles sampleApi",
-                ResponseType = "id_token token",
-                RedirectUri = "https://localhost:44319/",
+            //    ClientId = "mvc",
+            //    Scope = "openid profile roles sampleApi",
+            //    ResponseType = "id_token token",
+            //    RedirectUri = "https://localhost:44319/",
 
-                SignInAsAuthenticationType = "Cookies",
-                UseTokenLifetime = false,
+            //    SignInAsAuthenticationType = "Cookies",
+            //    UseTokenLifetime = false,
 
-                Notifications = new OpenIdConnectAuthenticationNotifications
-                {
-                    SecurityTokenValidated = async n =>
-                        {
-                            var nid = new ClaimsIdentity(
-                                n.AuthenticationTicket.Identity.AuthenticationType,
-                                Constants.ClaimTypes.GivenName,
-                                Constants.ClaimTypes.Role);
+            //    Notifications = new OpenIdConnectAuthenticationNotifications
+            //    {
+            //        SecurityTokenValidated = async n =>
+            //            {
+            //                var nid = new ClaimsIdentity(
+            //                    n.AuthenticationTicket.Identity.AuthenticationType,
+            //                    Constants.ClaimTypes.GivenName,
+            //                    Constants.ClaimTypes.Role);
 
-                                // get userinfo data
-                                var userInfoClient = new UserInfoClient(
-                                new Uri(n.Options.Authority + "/connect/userinfo"),
-                                n.ProtocolMessage.AccessToken);
+            //                    // get userinfo data
+            //                    var userInfoClient = new UserInfoClient(
+            //                    new Uri(n.Options.Authority + "/connect/userinfo"),
+            //                    n.ProtocolMessage.AccessToken);
 
-                            var userInfo = await userInfoClient.GetAsync();
-                            userInfo.Claims.ToList().ForEach(ui => nid.AddClaim(new Claim(ui.Item1, ui.Item2)));
+            //                var userInfo = await userInfoClient.GetAsync();
+            //                userInfo.Claims.ToList().ForEach(ui => nid.AddClaim(new Claim(ui.Item1, ui.Item2)));
 
-                                // keep the id_token for logout
-                                nid.AddClaim(new Claim("id_token", n.ProtocolMessage.IdToken));
+            //                    // keep the id_token for logout
+            //                    nid.AddClaim(new Claim("id_token", n.ProtocolMessage.IdToken));
 
-                                // add access token for sample API
-                                nid.AddClaim(new Claim("access_token", n.ProtocolMessage.AccessToken));
+            //                    // add access token for sample API
+            //                    nid.AddClaim(new Claim("access_token", n.ProtocolMessage.AccessToken));
 
-                                // keep track of access token expiration
-                                nid.AddClaim(new Claim("expires_at", DateTimeOffset.Now.AddSeconds(int.Parse(n.ProtocolMessage.ExpiresIn)).ToString()));
+            //                    // keep track of access token expiration
+            //                    nid.AddClaim(new Claim("expires_at", DateTimeOffset.Now.AddSeconds(int.Parse(n.ProtocolMessage.ExpiresIn)).ToString()));
 
-                                // add some other app specific claim
-                                nid.AddClaim(new Claim("app_specific", "some data"));
+            //                    // add some other app specific claim
+            //                    nid.AddClaim(new Claim("app_specific", "some data"));
 
-                            n.AuthenticationTicket = new AuthenticationTicket(
-                                nid,
-                                n.AuthenticationTicket.Properties);
-                        },
+            //                n.AuthenticationTicket = new AuthenticationTicket(
+            //                    nid,
+            //                    n.AuthenticationTicket.Properties);
+            //            },
 
-                    RedirectToIdentityProvider = n =>
-                        {
-                            if (n.ProtocolMessage.RequestType == OpenIdConnectRequestType.LogoutRequest)
-                            {
-                                var idTokenHint = n.OwinContext.Authentication.User.FindFirst("id_token");
+            //        RedirectToIdentityProvider = n =>
+            //            {
+            //                if (n.ProtocolMessage.RequestType == OpenIdConnectRequestType.LogoutRequest)
+            //                {
+            //                    var idTokenHint = n.OwinContext.Authentication.User.FindFirst("id_token");
 
-                                if (idTokenHint != null)
-                                {
-                                    n.ProtocolMessage.IdTokenHint = idTokenHint.Value;
-                                }
-                            }
+            //                    if (idTokenHint != null)
+            //                    {
+            //                        n.ProtocolMessage.IdTokenHint = idTokenHint.Value;
+            //                    }
+            //                }
 
-                            return Task.FromResult(0);
-                        }
-                }
-            });
+            //                return Task.FromResult(0);
+            //            }
+            //    }
+            //});
         }
 
         private void ConfigureIdentityProviders(IAppBuilder app, string signInAsType)
