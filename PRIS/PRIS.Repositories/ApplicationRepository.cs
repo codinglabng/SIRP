2193e543bdc68a6d4a0f7b5e1d2b1eeae2ca4b69
Project Manager <team@codinglab.ng>
2017-04-09 14:56:21 +0100

    Cloned AppManager branch

---
+++ PRIS/PRIS.Repositories/ApplicationRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PRIS.Core;
+using PRIS.Data.Models;
+
+namespace PRIS.Repositories
+{
+    public class ApplicationRepository : IApplicationRepositories
+    {
+        private readonly PrisDbEntities _db = new PrisDbEntities();
+        public void Add(App app)
+        {
+            _db.Apps.Add(app);
+            Save();
+        }
+
+        public IQueryable<App> GetApps()
+        {
+            var app = _db.Apps.OrderByDescending(e => e.Id);
+            return app;
+        }
+
+        public App GetAppsById(int Id)
+        {
+            var app = _db.Apps.FirstOrDefault(e => e.Id == Id);
+            return app;
+        }
+
+        public void Remove(App app)
+        {
+            _db.Apps.Remove(app);
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
